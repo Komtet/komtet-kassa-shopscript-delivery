b78ab7061e68f03aca83cd523ef85bcfc6795478
alexandr.motmom@gmail.com <alexandr.motmom@gmail.com>
2019-06-20 16:29:43 +0300

    convert tab to space

--- komtetdelivery/lib/config/install.php
+++ komtetdelivery/lib/config/install.php
@@ -2,9 +2,9 @@
 $model = new waModel();
 
 try {
-		$sql = "SELECT `fiscalised` FROM `shop_order` WHERE 0";
-		$model->query($sql);
+    $sql = "SELECT `fiscalised` FROM `shop_order` WHERE 0";
+    $model->query($sql);
 } catch (waDbException $ex) {
-		$sql = "ALTER TABLE `shop_order` ADD COLUMN `fiscalised` TINYINT(1) NOT NULL DEFAULT '0' AFTER `state_id`";
-		$model->query($sql);
+    $sql = "ALTER TABLE `shop_order` ADD COLUMN `fiscalised` TINYINT(1) NOT NULL DEFAULT '0' AFTER `state_id`";
+    $model->query($sql);
 }
