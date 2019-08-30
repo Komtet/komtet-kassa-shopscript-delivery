230a4404967b1400d4facdd8cd96bde622667b7d
awekening2 <alexandr.motmom@gmail.com>
2019-08-30 12:28:49 +0300

    именование таблицы

--- komtetdelivery/lib/config/db.php
+++ komtetdelivery/lib/config/db.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'komtet_delivery' => array(
+    'shop_komtet_delivery' => array(
         'order_id' => array('int', 11, 'null' => 0),
         'kk_id' => array('int', 11),
         'request' => array('text'),
